1713e7db6e8fa19a9b0d5db64847c7d3c271c27e
Robbie <resteinbock@gmail.com>
2014-09-16 20:10:42 -0500

    actual camera module

--- lib/photo_script.sh
+++ lib/photo_script.sh
@@ -4,8 +4,10 @@
 # script to take a picture
 #
 
-DATE=$(date +"%Y-%m-%d_%H%M")
+DATE=$(date +"%Y-%m-%d_%H%M_%S")
 
 echo $DATE
 
-fswebcam /home/pi/Documents/raspberry_akitabox_camera/.tmp/photos/$DATE.jpg
+# fswebcam /home/pi/Documents/raspberry_akitabox_camera/.tmp/photos/$DATE.jpg
+
+raspistill -o /home/pi/Documents/raspberry_akitabox_camera/.tmp/photos/$DATE.jpg
