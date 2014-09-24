bacf1eff8aede8a557384e565f352cfef1dee3e9
Robbie <resteinbock@gmail.com>
2014-09-24 12:54:02 -0500

    send pic after taking it

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ module.exports = function(app){
     app.all('/webhooks/settings', webhooks.settings);
     app.all('/settings/project', require('./settings/project')(app));
     app.all('/settings/account', require('./settings/account')(app));*/
+
     app.all('/', function(req, res, next){
         res.send("Raspberry Pi Camera App Running....");
     });
