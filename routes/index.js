9b1a7924b973d45f3b5679eba355dcf69ba4e92b
AkitaBot <info@akitabox.com>
2014-11-11 10:28:58 -0600

    add restart route

--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,5 @@ module.exports = function(app){
     app.get('/restartrpi', function(req, res, next){
         res.send("attempting to restart....");
         app.sys_checks.foreceRestart(function(err){ /*...*/ });
-    })
+    });
 };
