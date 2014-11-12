f5ac48e30f0023fa0a44f7fc1ba1e924f5fed8e7
AkitaBot <info@akitabox.com>
2014-11-11 22:43:50 -0600

    id

--- _util/mynodejsapplication.sh
+++ _util/mynodejsapplication.sh
@@ -43,26 +43,28 @@ NODE_BIN_DIR=/home/pi/node/node-v0.10.28-linux-arm-pi/bin
 NODE_PATH=/home/pi/node/node-v0.10.28-linux-arm-pi/lib/node_modules
 APPLICATION_DIRECTORY=/home/pi/Documents/raspberry_akitabox_camera
 APPLICATION_START=app.js
-PIDFILE=/var/run/$NAME.pid
+PID_FILE=$NAME.pid
+PID_DIR=/var/run
 LOGFILE=/home/pi/Documents/raspberry_akitabox_camera/logs/$NAME.log
 
 # If you want a command to always run, put it here
 PATH=$NODE_BIN_DIR:$PATH
+PID_PATH=$PID_DIR/$PID_FILE
 export NODE_PATH=$NODE_PATH
 export RPI_CONFIG=/home/pi/Documents/raspberry_akitabox_camera/lib/config_files/config.js
 export env=production
 
 start() {
     echo "Starting $NAME"
-    /home/pi/node/node-v0.10.28-linux-arm-pi/bin/forever --sourceDir $APPLICATION_DIRECTORY \
+    /home/pi/node/node-v0.10.28-linux-arm-pi/bin/forever -p $PID_DIR --pidFile $PID_FILE --sourceDir $APPLICATION_DIRECTORY \
         -a -l $LOGFILE --minUptime 5000 --spinSleepTime 2000 \
         start $APPLICATION_START &
     RETVAL=$?
 }
 
 stop() {
-    if [ -f $PIDFILE ]; then
-        echo "Shutting down $NAME $PIDFILE"
+    if [ -f $PID_PATH ]; then
+        echo "Shutting down $NAME $PID_FILE"
         forever stop $APPLICATION_START
         RETVAL=$?
     else
