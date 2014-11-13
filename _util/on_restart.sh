51a4fd5c10f44fcfb1596b35a676dd22d3518b93
AkitaBot <info@akitabox.com>
2014-11-13 12:18:59 -0600

    fix console.error

--- _util/on_restart.sh
+++ _util/on_restart.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # not needed
 # set env="production"
@@ -8,9 +9,14 @@
 #
 # sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 #
+# sudo crontab -e
 # SHELL=/bin/sh
 # PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 # @reboot /usr/local/bin/forever start -al /home/pi/Documents/raspberry_akitabox_camera/logs/camera.log /home/pi/Documents/raspberry_akitabox_camera/app.js
 #
 # sudo vim /etc/rc.local
-# su pi -c 'forever start -al /home/pi/Documents/raspberry_akitabox_camera/logs/camera.log /home/pi/Documents/raspberry_akitabox_camera/app.js < /dev/null &'
+# echo "Running forever script..."
+# su pi -c '/home/pi/node/node-v0.10.28-linux-arm-pi/bin/forever start -c /home/pi/node/node-v0.10.28-linux-arm-pi/bin/node -al /home/pi/Documents/raspberry_akitabox_camera/logs/camera.log /home/pi/Documents/raspberry_akitabox_camera/app.js < /dev/null &'
+#
+#
+# initd-forever -c /home/pi/node/node-v0.10.28-linux-arm-pi/bin/node -l /home/pi/Documents/raspberry_akitabox_camera/logs/camera.log -f /home/pi/node/node-v0.10.28-linux-arm-pi/bin
