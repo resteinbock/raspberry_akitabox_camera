7aa1c89d00ea485aa7354bcedff7cae1815f3f10
Robbie <resteinbock@gmail.com>
2015-02-01 16:52:47 -0600

    add bower

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.camera.configure(app);
 
 app.set('port', app.config.port || 3000);
 app.set('env', process.env.ENV || 'local');
-app.set('views', path.join(__dirname, 'public'));
+app.set('views', path.join(__dirname, 'public/templates'));
 app.set('view engine', 'hjs');
 
 app.use(express.logger('dev'));
