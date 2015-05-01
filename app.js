046db364789004006414e485c7757a1eaff7a70a
resteinbock <resteinbock@gmail.com>
2015-04-30 23:23:51 -0500

    tweak single page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ process.chdir(__dirname);
 var express = require('express');
 var http = require('http');
 var path = require('path');
-require('colors');
+var colors = require('colors');
 
 var app = express();
 
@@ -14,12 +14,30 @@ app.set('env', process.env.ENV || 'local');
 app.set('views', path.join(__dirname, 'public/templates'));
 app.set('view engine', 'hjs');
 
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 //Heartbeat middleware
 app.use( app.standard_middleware.heartbeat() );
+
+app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.logger('dev'));
+
+app.use(function(req, res, next) {
+    //enable CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, x-access-token, x-response-type, x-forwarded-proto, Origin, Accept, Accept-Language, Content-Language, Content-Type, Authorization, Content-Length');
+
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        return res.send(200);
+    }
+
+    return next();
+});
 
 app.use( app.routes.middleware() );
 app.routes.router(app);
@@ -32,10 +50,10 @@ app.use( app.standard_middleware.prodErrorHandler() );
 
 //Start the server
 http.createServer(app).listen(app.get('port'), function() {
-    //run ifconfig so that it will be posted to campfire
+    //run ifconfig so that the ip address will be posted to campfire
     var script = path.join(__dirname, '_util/ifconfig.sh');
     app.sys_checks.runScript(script, function(err){
-        //dont do anything with the error
+        //don't do anything with the error
         app.standard_middleware.onServerCreate();
     });
 });
